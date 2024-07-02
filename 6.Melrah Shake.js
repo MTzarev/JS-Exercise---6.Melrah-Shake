5497fbab7ce814d2a287b4fc5b2bf71ccf068224
Miroslav Tsarev <mtzarev@gmail.com>
2024-07-02 23:13:59 +0300

    Add files via upload

---
+++ 6.Melrah Shake.js
@@ -0,0 +1,21 @@
+function melrahShake(input) {
+    let pattern = input.pop()
+    let array = input.join(``)
+    let newArr = []
+    // let asciiNum = []
+    // for (let i = 0; i < array.length; i++) {
+    //     asciiNum.push(array[i].charCodeAt(array[i]))
+
+    // }
+for(let i=0; i<array.length;i++){
+    let i=
+}
+      
+    let result = array.replace(new RegExp(pattern, 'g'), '');
+    newArr.push(new RegExp(pattern, 'g'), '')
+    console.log(result); // Output: alavibaby
+    console.log(newArr);
+   
+}
+melrahShake(['astalavista baby', 'sta']);
+//melrahShake(['##mtm!!mm.mm*mtm.#','mtm']);
